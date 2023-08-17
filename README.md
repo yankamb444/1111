f6232c4abd89e7a4e8a005d7b5baef02c3eb2f52
Yandra Kamberaj <yandrakamb@gmail.com>
2023-08-17 13:52:08 -0400

    can prompt the user

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 1111
+
+
+https://cs.wellesley.edu/~cs307/readings/JS-objects.shtml
+
---
+++ index.js
@@ -0,0 +1,93 @@
+const inquirer = require('inquirer');
+const fs = require('fs-extra');
+const {
+    Triangle,
+    Circle,
+    Square
+} = require('./lib/shapes.js');
+
+var svg = require('svg-builder');
+// .width(300)
+// .height(200);
+
+
+
+async function main() {
+        await inquirer
+            .prompt([
+
+                {
+                    name: 'shape',
+                    type: 'list',
+                    message: 'Select Triangle, Square, or Circle',
+                    choices: ['Triangle', 'Circle', 'Square'],
+                },
+
+                {
+                    name: "userTxt",
+                    type: "input",
+                    message: "Type a three letter word",
+                },
+
+                {
+                    name: 'textColor',
+                    type: "list",
+                    message: "Select a color keyword",
+                    choices: ["#000000", "#00FFFF", "#0000FF"],
+                },
+
+                {
+                    name: 'shapeColor',
+                    type: "list",
+                    message: "Select the shape's color: #000000 is black, #00FFFF is blue, #0000FF is aqua",
+                    choices: ["	#000000", "	#00FFFF", "	#0000FF"],
+                },
+            ])
+            .then(answer => {
+                if (answer.shape === "Circle") {
+                    GenerateCircle()
+                } else if (answer.shape === "Triangle") {
+                    generateTriangle()
+                } else if (answer.shape === "Square") {
+                    generateSquare()
+                }
+
+            });
+
+        async function GenerateCircle(circle) {
+                        let circle = svg
+                            .circle([{
+                                r: 40,
+                                cx: 84,
+                                cy: 82,
+                            fill: ""}]);
+
+                               
+                            }
+                    };
+                
+
+
+
+                    // fs.appendFile('Logo.SVG', "", (err) => {
+                    //     if (err) console.log(err);
+                    //     else()
+                        
+                    // });
+
+
+
+                // var triangle1 = new Triangle("blue", 10, 10, 10)
+
+                // triangle1.printTriangle()
+
+
+                // triangle1.generateSvg()
+
+                main();
+                // Circle.prototype = Object.create(Shape.prototype);
+                // Circle.prototype.getRadius = function () {
+                //     return this.radius;
+
+
+                // fs.appendFile('Logo.SVG', function (generateSvg) 
